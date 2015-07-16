448c6afd06ea15a673cf169ba4c03aefefe424b5
rommy <rommy@microvac.co.id>
2015-07-16 17:05:10 +0700

    modified database + add some dummy data for belanja dan pendapatan

--- database/sideka.sql
+++ database/sideka.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 15, 2015 at 08:10 PM
+-- Generation Time: Jul 16, 2015 at 12:03 PM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -33,7 +33,21 @@ CREATE TABLE IF NOT EXISTS `tbl_akun` (
   `nama_akun` varchar(100) NOT NULL,
   `jumlah` int(30) NOT NULL,
   `keterangan` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `tbl_akun`
+--
+
+INSERT INTO `tbl_akun` (`id_akun`, `nomor`, `id_apbdes`, `nama_akun`, `jumlah`, `keterangan`) VALUES
+(1, '1', 1, 'Pendapatan Asli Desa', 3000000, 'Pendapatan Asli Desa'),
+(2, '2', 1, 'Pendapatan Transfer', 3000000, 'Pendapatan Transfer'),
+(3, '3', 1, 'Pendapatan Lain lain', 4000000, 'Pendapatan Lain lain'),
+(4, '4', 3, 'Penyelenggaraan Pemerintahan Desa', 20000000, 'Penyelenggaraan Pemerintahan Desa'),
+(5, '5', 3, 'Pelaksanaan Pembangunan Desa', 20000000, 'Pelaksanaan Pembangunan Desa'),
+(6, '6', 3, 'Pembinaan Kemasyarakatan', 20000000, 'Pembinaan Kemasyarakatan'),
+(7, '7', 3, 'Pemberdayaan Masyarakat', 20000000, 'Pemberdayaan Masyarakat'),
+(8, '', 3, 'Bidang Tak Terduga', 20000000, 'Bidang Tak Terduga');
 
 -- --------------------------------------------------------
 
@@ -46,8 +60,15 @@ CREATE TABLE IF NOT EXISTS `tbl_apbdes` (
   `tahun` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `is_perubahan` tinyint(1) NOT NULL,
   `nama` varchar(75) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
+--
+-- Dumping data for table `tbl_apbdes`
+--
+
+INSERT INTO `tbl_apbdes` (`id_apbdes`, `tahun`, `is_perubahan`, `nama`) VALUES
+(1, '2015-07-15 13:22:06', 0, 'Pendapatan'),
+(3, '2015-07-16 07:56:06', 0, 'Belanja');
 
 -- --------------------------------------------------------
 
@@ -92,12 +113,12 @@ ALTER TABLE `tbl_realisasi`
 -- AUTO_INCREMENT for table `tbl_akun`
 --
 ALTER TABLE `tbl_akun`
-MODIFY `id_akun` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `id_akun` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `tbl_apbdes`
 --
 ALTER TABLE `tbl_apbdes`
-MODIFY `id_apbdes` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `id_apbdes` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `tbl_realisasi`
 --
